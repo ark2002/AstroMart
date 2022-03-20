d441089fca0a200e23ddeb0fbcb88eeae6d914fd
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-03-20 16:41:08 +0530

    fix : bug fixes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import "./App.css";
-import { Footer } from "./frontend/components/Footer/Footer";
-import { Navbar } from "./frontend/components/Navbar/Navbar";
-import { Homescreen } from "./frontend/screens/Homescreen/Homescreen";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Homescreen/>
-      <Footer />
     </div>
   );
 }
