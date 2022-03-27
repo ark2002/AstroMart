ee313586937e0c40878f4970b0d4322a0bf92256
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-03-27 13:21:43 +0530

    chore : clean ups

--- src/frontend/services/auth-services/signInService.js
+++ src/frontend/services/auth-services/signInService.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const signInService = async (user) => {
-    console.log(user);
     try {
         const { data } = await axios.post("/api/auth/login", {
             email: user.email,
--- src/frontend/services/auth-services/signUpService.js
+++ src/frontend/services/auth-services/signUpService.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const signUpService = async (user) => {
-    console.log(user);
     try {
         const { data } = await axios.post("/api/auth/signup", {
             firstName: user.firstName,
