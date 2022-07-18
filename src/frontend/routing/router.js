9bce4d63313ea46e5567be749d04f1ffab732044
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-07-18 18:59:52 +0530

    refac: Router

--- src/frontend/routing/router.js
+++ src/frontend/routing/router.js
@@ -2,7 +2,8 @@ import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 
 import { PrivateRoute } from "../components";
-import { useAuth } from "../contexts"
+import { useAuth } from "../contexts";
+import { ScrollToTop } from "../hooks/ScrollToTop";
 import {
   CartScreen,
   HomeScreen,
@@ -13,21 +14,39 @@ import {
 } from "../screens";
 
 const Router = () => {
-  const { auth: { status }, } = useAuth();
+  const {
+    auth: { status },
+  } = useAuth();
   return (
-    <Routes>
-      <Route path="/" element={<HomeScreen />} />
-      <Route path="/products" element={<ProductsScreen />} />
-      <Route path="/cart" element={<PrivateRoute><CartScreen /></PrivateRoute>} />
-      <Route path="/wishlist" element={<PrivateRoute><WishlistScreen /></PrivateRoute>} />
-      {!status && (
-        <>
-          <Route path="/signin" element={<SignInScreen />} />
-          <Route path="/signup" element={<SignUpScreen />} />
-        </>
-      )}
-      <Route path="*" element={<Navigate replace to="/" />} />
-    </Routes>
+    <ScrollToTop>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/products" element={<ProductsScreen />} />
+        <Route
+          path="/cart"
+          element={
+            <PrivateRoute>
+              <CartScreen />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/wishlist"
+          element={
+            <PrivateRoute>
+              <WishlistScreen />
+            </PrivateRoute>
+          }
+        />
+        {!status && (
+          <>
+            <Route path="/signin" element={<SignInScreen />} />
+            <Route path="/signup" element={<SignUpScreen />} />
+          </>
+        )}
+        <Route path="*" element={<Navigate replace to="/" />} />
+      </Routes>
+    </ScrollToTop>
   );
 };
 export { Router };
