8d3f8f993824b1105bca9e26ce9805f44c7bcd3d
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-08-03 21:04:54 +0530

    feat : updated SignInScreen

--- src/frontend/screens/index.js
+++ src/frontend/screens/index.js
@@ -1,6 +1,8 @@
-export { HomeScreen } from "./HomeScreen/HomeScreen"
-export { ProductsScreen } from "./ProductsScreen/ProductsScreen"
-export { CartScreen } from "./CartScreen/CartScreen"
-export { WishlistScreen } from "./WishlistScreen/WishlistScreen"
-export { SignInScreen } from "./SignInScreen/SignInScreen"
-export { SignUpScreen } from "./SignUpScreen/SignUpScreen"
+export { HomeScreen } from "./HomeScreen/HomeScreen";
+export { ProductsScreen } from "./ProductsScreen/ProductsScreen";
+export { CartScreen } from "./CartScreen/CartScreen";
+export { WishlistScreen } from "./WishlistScreen/WishlistScreen";
+export { SignInScreen } from "./SignInScreen/SignInScreen";
+export { SignUpScreen } from "./SignUpScreen/SignUpScreen";
+export { CheckOutScreen } from "./CheckOutScreen/CheckOutScreen";
+export { OrderSummaryScreen } from "./OrderSummaryScreen/OrderSummaryScreen";
